4859d9ac4842a70a8b3798988941ffe826168aa5
timr <timr@thestowcompany.com>
2016-04-04 16:54:52 -0400

    Started on the react modal

--- tournaments/static/js/home_babel.js
+++ tournaments/static/js/home_babel.js
@@ -7,7 +7,7 @@ var data = [
             'D1 - Li, Wei',
             'D2 - Huang, Hanye',
             'D3 - Emborsky, Eric',
-            'D4 - Cvetkovski, Steven',
+            'D4 - Cvetkovski, Steven'
         ],
         opponents: [
             '(1894) Lu, Xiaotian',
@@ -22,10 +22,17 @@ var data = [
 var ActionItem = React.createClass({
     handleClick: function(event) {
         console.log(this.props.roster_data);
+        ReactDOM.render(
+            <RosterModal roster={this.props.roster_data}/>, 
+            document.getElementById('modal-container')
+        );
+        $('#modal1').openModal();
+        $('ul.tabs').tabs();
+        $('select').material_select();
     },
     render: function() {
         return (
-            <a href="#!" className="collection-item roster-fill-in" onClick={this.handleClick}>
+            <a href="#!" className="collection-item rooster-fill-in" onClick={this.handleClick}>
                 {this.props.roster_data.itemText}
                 <i className="secondary-content mdi-content-send"></i>
             </a>
@@ -52,7 +59,77 @@ var ActionItemsList = React.createClass({
     }
 }); 
 
+
+var RosterModal = React.createClass({
+    render: function() {
+        var opponents = this.props.roster.opponents.map(function(opponent, i) {
+            return (
+                <li className="collection-item">{opponent}</li>
+            );
+        });
+
+        var players = this.props.roster.players.map(function(player, i) {
+            return (
+                <a href="#!" className="collection-item">{player}<span className="badge">{i+1}</span></a>
+            );
+        });
+
+        var playerOptions = this.props.roster.players.map(function(player, i) {
+            if (i > 0) {
+                return (
+                    <option value="{i+1}">{player}</option>
+                );
+            }
+        });
+        
+        return (
+            <div id="modal1" className="modal modal-fixed-footer">
+                <div className="modal-content">
+                    <div className="row center-align valign-wrapper">
+                        <div className="col l5 s5"><h5>{this.props.roster.leftTeamName}</h5></div>
+                        <div className="col l2 s2 valign">vs.</div>
+                        <div className="col l5 s5"><h5>{this.props.roster.rightTeamName}</h5></div>
+                    </div>
+                    <div className="row">
+                        <div className="col s12">
+                            <ul className="tabs">
+                                <li className="tab col s6"><a className="active" href="#roster-players-tab">Players</a></li>
+                                <li className="tab col s6"><a href="#roster-opponents-tab">Opponents</a></li>
+                            </ul>
+                        </div>
+                        <div id="roster-players-tab" className="col s12">
+                            <div className="collection">
+                                {players}
+                            </div>
+
+                            <h5>Doubles</h5>
+                            <ul id="doubles-list" className="collection">
+                                <li className="collection-item">D1 - Li, Wei</li>
+                                <li className="collection-item">
+                                    <select value="1">
+                                        <option value="" disabled>Select doubles partner</option>
+                                        {playerOptions}
+                                    </select>
+                                </li>
+                            </ul>
+                        </div>
+                        <div id="roster-opponents-tab" className="col s12">
+                            <ul className="collection">
+                                {opponents}
+                            </ul>
+                        </div>
+                    </div>            
+                </div>
+                <div className="modal-footer">
+                    <a href="#" className="waves-effect waves-green btn-flat modal-action modal-close">Submit Roster</a>
+                </div>
+            </div>
+        );
+    }
+});
+
 ReactDOM.render(
     <ActionItemsList data={data} />, 
     document.getElementById('action-items')
 );
+
